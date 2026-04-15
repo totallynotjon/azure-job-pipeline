01c2fa2cdf29e69c41f30fb6b259eacabcc528a1
Jon B <brostash1@gmail.com>
2026-04-15 09:47:26 -0400

    remove dependencies for auto managed providers

--- infra/identity.tf
+++ infra/identity.tf
@@ -2,7 +2,6 @@ resource "azurerm_user_assigned_identity" "scraper_job" {
   name                = "id-scraper-job"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  depends_on          = [azurerm_resource_provider_registration.managed_identity]
 
   tags = var.default_project_tags
 }
