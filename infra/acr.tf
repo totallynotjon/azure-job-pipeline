c015d0bed4a625c923590ff8dcf6ead9a1590f33
Jon B <brostash1@gmail.com>
2026-04-15 00:18:03 -0400

    explicit resource registration with depends

--- infra/acr.tf
+++ infra/acr.tf
@@ -4,6 +4,7 @@ resource "azurerm_container_registry" "main" {
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
   admin_enabled       = false
+  depends_on          = [azurerm_resource_provider_registration.container_registry]
 
   tags = var.default_project_tags
 }
